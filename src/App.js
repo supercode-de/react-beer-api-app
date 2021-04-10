f1807bea904014cea3407005153d631acff12211
david <kontakt@davidbigus.de>
2021-04-10 10:06:43 +0200

    view changed

--- src/App.js
+++ src/App.js
@@ -5,21 +5,26 @@ import { Component } from 'react';
 
 export default class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      beers: []
+    }
+  }
+
   componentDidMount() {
-    
+
     // Get 1 beer
     getBeer('5fb6a86265b9c209606e10e2').then(data => console.log(data));
 
     // Get all beers
-    getAllBeers().then(data => console.log(data))
+    getAllBeers().then(data => this.setState({beers: data}));
 
     // Get a random beer
     getRandomBeer().then(data => console.log(data))
   }
 
   render() {
-    return (
-      <div></div>
-    )
+    return (<ul>{ this.state.beers.map((beer, index) => <li key={index}>{ beer.name }</li>) }</ul>)
   }
 }
